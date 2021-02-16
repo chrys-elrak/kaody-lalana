307820691a568ea5e71e045360494ca885dabc3e
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-02-16 18:01:53 +0300

    update i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +6,6 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('mg');
-}
+  constructor() {
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,7 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class HomePage {
 
   constructor(translate: TranslateService) {
-    translate.setDefaultLang('mg');
+    translate.setDefaultLang('fr');
 }
 
 
