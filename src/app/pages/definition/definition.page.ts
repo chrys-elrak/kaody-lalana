99a142775693f868f78fcb179be3b5908030cebb
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-02-18 08:39:23 +0300

    open details modal

--- src/app/pages/definition/definition.page.ts
+++ src/app/pages/definition/definition.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DetailsModalComponent } from 'src/app/components/details-modal/details-modal.component';
 
 @Component({
   selector: 'app-definition',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./definition.page.scss'],
 })
 export class DefinitionPage implements OnInit {
+  text = `<b>Hello</b>`
 
-  constructor() {
-}
+  constructor(
+    private modalController: ModalController
+  ) {
+
+  }
+
   ngOnInit() {
+  }
+
+  async presentModal(data: any) {
+    const modal = await this.modalController.create({
+      component: DetailsModalComponent,
+      componentProps: data
+    });
+    return await modal.present();
   }
 
 }
